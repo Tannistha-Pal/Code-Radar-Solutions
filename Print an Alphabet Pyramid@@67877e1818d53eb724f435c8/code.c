6bdda2d8435b62c601235fd6548ae486cb6b7a9e
Tannistha-Pal <tannisthapal6@gmail.com>
2025-02-11 16:39:10 +0530

    Solution to Print an Alphabet Pyramid

--- Print an Alphabet Pyramid@@67877e1818d53eb724f435c8/code.c
+++ Print an Alphabet Pyramid@@67877e1818d53eb724f435c8/code.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 int main() {
-    int i, j, k, rows;
-    char ch;
-    // Get the number of rows from the user
-    scanf("%d", &rows);
-    for (i = 1; i <= rows; i++) {
-        // Print spaces for alignment
-        for (k = rows; k > i; k--) {
-            printf(" ");
-        }
-        ch = 'A'; // Start with 'A' for each row
-        // Print increasing sequence of alphabets
-        for (j = 1; j <= (2 * i - 1); j++) {
-            printf("%c", ch);
-            if (j < i)
-                ch++; // Increment character
-            else
-                ch--; // Decrement character
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        char ch = 'A';
+        for (int j = 0; j <= i; j++) {
+            printf("%c ", ch++);
         }
         printf("\n");
     }
