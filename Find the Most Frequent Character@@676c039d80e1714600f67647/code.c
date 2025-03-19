fe3d23694f911dd05c9856f00b5eedf3558b7ea7
TANNISTHA PAL <tannisthapal6@gmail.com>
2025-03-19 18:01:35 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -7,16 +7,19 @@
 char mostFrequentChar(char str[]) {
     int freq[ASCII_SIZE] = {0}; // Array to store frequency of characters
     int maxFreq = 0;
-    char mostFrequent = '\0';
+    char mostFrequent = 127; // Set to a high ASCII value initially
 
     // Count occurrences of each character
     for (int i = 0; str[i] != '\0'; i++) {
-        freq[(unsigned char)str[i]]++; // Use unsigned char to handle extended ASCII
+        if (str[i] != ' ') { // Ignore spaces
+            freq[(unsigned char)str[i]]++;
 
-        // Update most frequent character if needed
-        if (freq[(unsigned char)str[i]] > maxFreq) {
-            maxFreq = freq[(unsigned char)str[i]];
-            mostFrequent = str[i];
+            // Update most frequent character if needed
+            if (freq[(unsigned char)str[i]] > maxFreq || 
+                (freq[(unsigned char)str[i]] == maxFreq && str[i] < mostFrequent)) {
+                maxFreq = freq[(unsigned char)str[i]];
+                mostFrequent = str[i];
+            }
         }
     }
 
