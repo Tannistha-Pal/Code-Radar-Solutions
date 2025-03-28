0f0b6625cc69e126774acba5317b4b2b9930f4b0
TANNISTHA PAL <tannisthapal6@gmail.com>
2025-03-28 19:23:40 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,1 +1,18 @@
-// Your code here...
+#include <stdio.h>
+int main() {
+    int n, t, arr[100];
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    
+    scanf("%d", &t);
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == t)
+                printf("%d %d\n", arr[i], arr[j]);
+        }
+    }   
+    return 0;
+}
